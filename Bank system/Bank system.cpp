9a797b6b9641f7ec3b38fb9a9a49ec1b6ee56d28
Mahmoud Elatar <62507670+mahmoudalatar@users.noreply.github.com>
2022-06-01 17:54:59 +0200

    uncommite

--- Bank system/Bank system.cpp
+++ Bank system/Bank system.cpp
@@ -20,40 +20,40 @@ using namespace std;
 
 int main()
 {
-	//::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+	::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
 
-	//cout << "                                                 **" << endl;
-	//cout << "                                                 **" << endl;
-	//cout << "                                                 **" << endl;
-	//cout << "                                                 **" << endl;
-	//cout << "                                                 **" << endl;
-	//cout << "**                ****                  **       **         ********                **********              **********                   ****                ****                ********        " << endl;
-	//cout << " **              **  **                **        **        **      **             **                       **        **                 **  **              **  **              **      **       " << endl;
-	//cout << "  **            **    **              **         **       **        **           **                       **          **               **    **            **    **            **        **      " << endl;
-	//cout << "   **          **      **            **          **      *************          **                       **            **             **      **          **      **           *************     " << endl;
-	//cout << "    **        **        **          **           **      **                    **                       **              **           **        **        **        **          **                " << endl;
-	//cout << "     **      **          **        **            **      **                    **                       **              **          **          **      **          **         **                " << endl;
-	//cout << "      **    **            **      **             **       **                    **                       **            **          **            **    **            **         **               " << endl;
-	//cout << "       **  **               **   **              **        **      **            **                       **          **          **              **  **              **         **      **      " << endl;
-	//cout << "        ****                 ****                **         ********               ***********             ************          **                ****                **         ********       " << endl;
+	cout << "                                                 **" << endl;
+	cout << "                                                 **" << endl;
+	cout << "                                                 **" << endl;
+	cout << "                                                 **" << endl;
+	cout << "                                                 **" << endl;
+	cout << "**                ****                  **       **         ********                **********              **********                   ****                ****                ********        " << endl;
+	cout << " **              **  **                **        **        **      **             **                       **        **                 **  **              **  **              **      **       " << endl;
+	cout << "  **            **    **              **         **       **        **           **                       **          **               **    **            **    **            **        **      " << endl;
+	cout << "   **          **      **            **          **      *************          **                       **            **             **      **          **      **           *************     " << endl;
+	cout << "    **        **        **          **           **      **                    **                       **              **           **        **        **        **          **                " << endl;
+	cout << "     **      **          **        **            **      **                    **                       **              **          **          **      **          **         **                " << endl;
+	cout << "      **    **            **      **             **       **                    **                       **            **          **            **    **            **         **               " << endl;
+	cout << "       **  **               **   **              **        **      **            **                       **          **          **              **  **              **         **      **      " << endl;
+	cout << "        ****                 ****                **         ********               ***********             ************          **                ****                **         ********       " << endl;
 
-	//cout << endl << endl << endl << "                                                                                 Press Enter To Start The programe " << endl;
-	//cin.get();
-	//system("cls");
-	//cout << "loading...." << endl;
-	////initializing
-	//bool initialized = false;
-	//if (!initialized) {
-	//	string files[3] = { "clientId.txt" ,"employeeLastId.txt" , "adminLastId.txt" };
-	//	initialized = true;
-	//	for (int i = 0; i < 3; i++) {
-	//	fstream file(files[i], ios::out);
-	//	file << 1;
-	//	file.close();
-	//	}
-	//}
-	//Sleep(2000);
-	//system("cls");
+	cout << endl << endl << endl << "                                                                                 Press Enter To Start The programe " << endl;
+	cin.get();
+	system("cls");
+	cout << "loading...." << endl;
+	//initializing
+	bool initialized = false;
+	if (!initialized) {
+		string files[3] = { "clientId.txt" ,"employeeLastId.txt" , "adminLastId.txt" };
+		initialized = true;
+		for (int i = 0; i < 3; i++) {
+		fstream file(files[i], ios::out);
+		file << 1;
+		file.close();
+		}
+	}
+	Sleep(2000);
+	system("cls");
 
 	Screen::menuSystem();
 }
